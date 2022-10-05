ad7ac18a2aa3fde849004298969e095254e46a4e
Contributor <contributor@tethys-framework.de>
2022-10-05 20:26:18 +0200

    geocode: order of results

--- geocode/logic.js
+++ geocode/logic.js
@@ -1,56 +1,150 @@
 let geocode_counter;
 let geocode_results;
+let geocode_results_counter;
+let geocode_service;
 function geocode(myin){
+
+    let radioValue = $("input[name='service']:checked").val();
+    if(!radioValue){
+        progress("Please specify service!");
+        return;
+    }
+    geocode_service = radioValue;
+
     if(!(myin.trim())){
         progress("0/0");
         return;
     }
+
     geocode_results = [];
+    geocode_results_counter = 0;
     let inArray = myin.trim().split("\n");
     progress("0/"+inArray.length);
     if (inArray.length===1){
         geocode_counter = 1;
-        geocode_single(inArray[0].trim(),5, geocode_done);
+        geocode_single(inArray[0].trim(),10, function(data){geocode_done(data, 0);});
         return;
     }
     geocode_counter = 0;
     for (let i = 0; i < inArray.length; i++){
         if(inArray[i].trim()){
             geocode_counter++;
-            geocode_single(inArray[i].trim(), 1, geocode_done);
+            geocode_single(inArray[i].trim(), 1, function(data){geocode_done(data, i);});
         }
     }
 }
-function geocode_done(data){
-    geocode_results.push(geocode_process(data));
-    progress(geocode_results.length+"/"+geocode_counter);
-    if(geocode_results.length===geocode_counter){
+function geocode_done(data, id){
+    geocode_results[id] = geocode_process(data);
+    geocode_results_counter++;
+    progress(geocode_results_counter+"/"+geocode_counter);
+    //if(geocode_results.length===geocode_counter)
+    {
         done(geocode_results.join("\n"));
     }
 }
 function geocode_process(data){
+    if(geocode_service==='nominatim'){
+        return geocode_line_array(data);
+    }
+    if(geocode_service==='geokeo'){
+        return geocode_line_array(data.results);
+    }
+    if(geocode_service==='xyz'){
+        return geocode_line_array([data]);
+    }
+    if(geocode_service==='geoapify'){
+        return geocode_line_array(data.features);
+    }
+    return null;
+}
+function geocode_line_array(data){
     if (geocode_counter===1){
-        return geocode_line_detail(data);
+        let result = [];
+        for (let i = 0; i < data.length; i++){
+            result.push(geocode_line_simple(data[i]));
+        }
+        return result.join("\n");
     }
     return geocode_line_simple(data[0]);
 }
-function geocode_line_detail(data){
-    let result = [];
-    for (let i = 0; i < data.length; i++){
-        let row = data[i];
-        result.push(row.display_name
-            +"\t"+row.lat+","+row.lon
-        );
-    }
-    return result.join("\n");
-}
 function geocode_line_simple(data){
-    return data.display_name+"\t"+data.lat+","+data.lon;
+    if(geocode_service==='nominatim'){
+        return data.type
+            +"\t"+data.address.road
+            +"\t"+data.address.house_number
+            +"\t"+data.address.postcode
+            +"\t"+data.address.city
+            +"\t"+data.address.country
+            +"\t"+geocode_format_latlon(data.lat,data.lon);
+    }
+    if(geocode_service==='geokeo'){
+        return data.class+"/"+data.type
+            +"\t"+data.address_components.street
+            +"\t"//+data.address_components.house_number
+            +"\t"+data.address_components.postcode
+            +"\t"+data.address_components.city
+            +"\t"+data.address_components.country
+            +"\t"+geocode_format_latlon(data.geometry.location.lat,data.geometry.location.lng);
+    }
+    if(geocode_service==='xyz'){
+        return ""
+            +"\t"+data.standard.addresst
+            +"\t"+data.standard.stnumber
+            +"\t"+data.standard.postal
+            +"\t"+data.standard.city
+            +"\t"+data.standard.countryname
+            +"\t"+geocode_format_latlon(
+                data.latt,
+                data.longt
+            );
+    }
+    if(geocode_service==='geoapify'){
+        return data.properties.result_type
+            +"\t"+data.properties.street
+            +"\t"+data.properties.housenumber
+            +"\t"+data.properties.postcode
+            +"\t"+(data.properties.city?data.properties.city:data.properties.suburb)
+            +"\t"+data.properties.country
+            +"\t"+geocode_format_latlon(
+                data.properties.lat,
+                data.properties.lon
+            );
+    }
+    return null;
+}
+function geocode_format_latlon(lat,lon){
+    lat = (lat*1).toFixed(7);
+    lon = (lon*1).toFixed(7);
+    return lat+","+lon;
 }
 function geocode_single(address, limit=1, callback=function(data){console.log(data);}){
-    request_json("https://nominatim.openstreetmap.org/search", {
-        "format": "jsonv2",
-        "q": address,
-        "limit": limit,
-    }, callback);
+    if(geocode_service==='nominatim'){
+        request_json("https://nominatim.openstreetmap.org/search", {
+            "format": "jsonv2",
+            "q": address,
+            "limit": limit,
+            "addressdetails": 1,
+            //"email": "...",
+        }, callback);
+    }
+    if(geocode_service==='geokeo'){
+        request_json("https://geokeo.com/geocode/v1/search.php", {
+            "api": "YOUR_API_KEY",
+            "q": address,
+        }, callback);
+    }
+    if(geocode_service==='xyz'){
+        request_json("https://geocode.xyz/"+encodeURIComponent(address), {
+            "auth": "...",
+            "json": 1,
+        }, callback);
+    }
+    if(geocode_service==='geoapify'){
+        request_json("https://api.geoapify.com/v1/geocode/search", {
+            "text": address,
+            "apiKey": apikey_geoapify,
+            "format": "geojson",
+            "limit": limit,
+        }, callback);
+    }
 }
--- logic.js
+++ logic.js
@@ -9,11 +9,51 @@ function progress(progressHtml){
     $("#progress").html(progressHtml);
 }
 function request_json(url, args={}, callback=function(data){console.log(data);}){
-    //url="demo.json";
+    console.log(url+"?"+buildGetString(args));
+    // return;
+    // url="/test.php";
+    // if(geocode_service==='nominatim'){ url="demo_nominatim.json"; }
+    // if(geocode_service==='geokeo'){ url="demo_geokeo.json"; }
+
+    // $.ajaxSetup({
+    //     beforeSend: function(request) {
+    //         request.setRequestHeader("Access-Control-Allow-Origin", '*');
+    //     }
+    // });
     $.ajax({
+
+        // //https://stackoverflow.com/questions/3372962/can-i-remove-the-x-requested-with-header-from-ajax-requests
+        // // 'xhr' option overrides jQuery's default
+        // // factory for the XMLHttpRequest object.
+        // // Use either in global settings or individual call as shown here.
+        // xhr: function() {
+        //     // Get new xhr object using default factory
+        //     let xhr = jQuery.ajaxSettings.xhr();
+        //     // Copy the browser's native setRequestHeader method
+        //     let setRequestHeader = xhr.setRequestHeader;
+        //     // Replace with a wrapper
+        //     xhr.setRequestHeader = function(name, value) {
+        //         // Ignore the X-Requested-With header
+        //         if (name === 'X-Requested-With') return;
+        //         // Otherwise call the native setRequestHeader method
+        //         // Note: setRequestHeader requires its 'this' to be the xhr object,
+        //         // which is what 'this' is here when executed.
+        //         setRequestHeader.call(this, name, value);
+        //     }
+        //     // pass it on to jQuery
+        //     return xhr;
+        // },
+
         dataType: "json",
         url: url,
         data: args,
         success: callback
     });
+}
+function buildGetString(args){
+    let string = [];
+    for (const [key, value] of Object.entries(args)) {
+        string.push(encodeURIComponent(key)+"="+encodeURIComponent(value));
+    }
+    return string.join("&");
 }
